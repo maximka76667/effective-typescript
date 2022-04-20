8dac5ee50885c603df550b43e1fbf9ac75dbd8de
maximka76667 <74974283+maximka76667@users.noreply.github.com>
2022-04-20 11:18:21 +0200

    Update I20.ts

--- src/C3/I20.ts
+++ src/C3/I20.ts
@@ -88,5 +88,6 @@ fetchProduct(id2);
 // Things to Remember
 
 // • While a variable’s value can change, its type generally does not.
+
 // • To avoid confusion, both for human readers and for the type checker, avoid reusing
 // variables for differently typed values.
